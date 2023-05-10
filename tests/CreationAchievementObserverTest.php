542a9a50e1249dfe3fa0fe47f446249b5fe75894
igorMSoares <igorms.dev@gmail.com>
2023-05-10 00:40:12 -0300

    test: create CreationAchievementObserverTest

---
+++ tests/CreationAchievementObserverTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Igormsoares\CourseraDesignPatterns;
+
+use PHPUnit\Framework\TestCase;
+
+final class CreationAchievementObserverTest extends TestCase
+{
+  public function testAddingInventorBadgeOn100CreationPoints(): void
+  {
+    $user = 'user1';
+
+    AchievementStorageFactory::setAchievementStorage(new MemoryAchievementStorage());
+    $achievementStorage = AchievementStorageFactory::getAchievementStorage();
+
+    $creationObserver = new CreationAchievementObserver($achievementStorage);
+
+    $creationAchievement = new Points('CREATION', 100);
+    $achievementStorage->addAchievement($user, $creationAchievement);
+
+    $creationObserver->achievementUpdate($user, $creationAchievement);
+
+    $inventorBadge = $achievementStorage->getAchievement($user, 'INVENTOR');
+
+    $this->assertTrue($inventorBadge instanceof Badge);
+  }
+}
