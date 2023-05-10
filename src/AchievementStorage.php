d9e1c681998355b7349221675e07c8557fc7f4b4
igorMSoares <igorms.dev@gmail.com>
2023-05-10 17:23:11 -0300

    fix: attachAchievementObserver() must be present in the superclass AchievementStorage

--- src/AchievementStorage.php
+++ src/AchievementStorage.php
@@ -6,6 +6,8 @@ namespace Igormsoares\CourseraDesignPatterns;
 
 interface AchievementStorage
 {
+  public function attachAchievementObserver(AchievementObserver $o): void;
+
   public function addAchievement(string $user, Achievement $a): void;
 
   /**
