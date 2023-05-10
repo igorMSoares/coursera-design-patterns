60873e48b3c41a46bc4ac5247dece2635c592338
igorMSoares <igorms.dev@gmail.com>
2023-05-09 23:48:03 -0300

    test: add tests to verify Badges and Points after multiple  ForumServiceGamificationProxy's functions calls

--- tests/ForumServiceGamificationProxyTest.php
+++ tests/ForumServiceGamificationProxyTest.php
@@ -57,6 +57,7 @@ final class ForumServiceGamificationProxyTest extends TestCase
     $this->assertSame($participationPoints, $points);
     $this->assertSame($badgeName, $badge);
   }
+
   public function testForumServiceAddingBadgeAndPointsOnLikeTopic(): void
   {
     $user = 'user1';
@@ -74,5 +75,84 @@ final class ForumServiceGamificationProxyTest extends TestCase
       $points = $pointsAchievement->getTotalPoints();
     }
     $this->assertSame($creationPoints, $points);
+  }
+
+  public function testForumServiceAddingBadgeAndPointsOnAddTopicTwice(): void
+  {
+    $user = 'user1';
+
+    AchievementStorageFactory::setAchievementStorage(new MemoryAchievementStorage());
+    $forumService = new ForumServiceGamificationProxy(new ForumServiceGamification());
+    $forumService->addTopic($user, 'firstTopic');
+    $forumService->addTopic($user, 'secondTopic');
+
+    $creationPoints = 10;
+    $badgeName = 'I CAN TALK';
+
+    $achievementStorage = AchievementStorageFactory::getAchievementStorage();
+    $pointsAchievement = $achievementStorage->getAchievement($user, 'CREATION');
+    $badgeAchievement = $achievementStorage->getAchievement($user, $badgeName);
+
+    if ($pointsAchievement instanceof Points) {
+      $points = $pointsAchievement->getTotalPoints();
+    }
+
+    $this->assertSame($creationPoints, $points);
+    $this->assertSame($badgeName, $badgeAchievement->getName());
+    $this->assertCount(2, $achievementStorage->getAchievements($user));
+  }
+
+  public function testForumServiceAddingBadgeAndPointsOnMultipleFunctionsCalls(): void
+  {
+    $user = 'user1';
+
+    AchievementStorageFactory::setAchievementStorage(new MemoryAchievementStorage());
+    $forumService = new ForumServiceGamificationProxy(new ForumServiceGamification());
+
+    // 5 CREATION pts + I CAN TALK
+    $forumService->addTopic($user, 'firstTopic');
+
+    // 3 PARTICIPATION pts + LET ME ADD
+    $forumService->addComment($user, 'firstTopic', 'comment1');
+    // 3 PARTICIPATION pts
+    $forumService->addComment($user, 'firstTopic', 'comment2');
+
+    $forumService->likeTopic($user, 'firstTopic', 'user2'); // 1 CREATION pts
+    $forumService->likeTopic($user, 'secondTopic', 'user2'); // 1 CREATION pts
+    $forumService->likeTopic($user, 'thirdTopic', 'user2'); // 1 CREATION pts
+
+    // 1 PARTICIPATION pts
+    $forumService->likeComment($user, 'firstTopic', 'comment1', 'user2');
+    // 1 PARTICIPATION pts
+    $forumService->likeComment($user, 'firstTopic', 'comment2', 'user2');
+    // 1 PARTICIPATION pts
+    $forumService->likeComment($user, 'firstTopic', 'comment3', 'user2');
+
+    $creationPoints = 8;
+    $participationPoints = 9;
+    $talkBadgeName = 'I CAN TALK';
+    $addBadgeName = 'LET ME ADD';
+
+    $achievementStorage = AchievementStorageFactory::getAchievementStorage();
+    $creationAchievement = $achievementStorage->getAchievement($user, 'CREATION');
+    $participationAchievement = $achievementStorage->getAchievement($user, 'PARTICIPATION');
+    $talkBadgeAchievement = $achievementStorage->getAchievement($user, $talkBadgeName);
+    $addBadgeAchievement = $achievementStorage->getAchievement($user, $addBadgeName);
+
+    if ($creationAchievement instanceof Points) {
+      $totalCreationPoints = $creationAchievement->getTotalPoints();
+    }
+
+    if ($participationAchievement instanceof Points) {
+      $totalParticipationPoints = $participationAchievement->getTotalPoints();
+    }
+
+    $this->assertSame($creationPoints, $totalCreationPoints);
+    $this->assertSame($participationPoints, $totalParticipationPoints);
+    $this->assertSame($talkBadgeName, $talkBadgeAchievement->getName());
+    $this->assertSame($addBadgeName, $addBadgeAchievement->getName());
+
+    // 2 Badges + 1 CREATION + 1 PARTICIPATION
+    $this->assertCount(4, $achievementStorage->getAchievements($user));
   }
 }
